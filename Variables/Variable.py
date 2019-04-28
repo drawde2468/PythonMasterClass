08368ace624c1dee3dfece7cbcc3b0c4699cfed9
Edward Leslie <edwardleslie09@gmail.com>
2019-04-28 01:39:48 -0400

    completed lesson 27

---
+++ Variables/Variable.py
@@ -0,0 +1,29 @@
+# greeting = "Bruce"
+# _myName = "Ed"
+# Ed45 = "Good"
+# Tim_Was_57 = "Hello"
+# Greeting = "There"
+#
+# print(Tim_Was_57 + " " + greeting)
+#
+# age = 24
+# print(age)
+#
+# print(greeting + " " + str(age))
+
+a = 12
+b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+#
+# for i in range(1, a // b):
+#     print(i)
+
+print(a + b / 3 - 4 * 12)
+print(a / 2 * 3)
+print(8 * 3 / 2)
+print((((a + b) / 3) - 4) * 12)
