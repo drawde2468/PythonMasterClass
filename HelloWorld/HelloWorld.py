3534091f8d2db009abfde3e73fc42775696e2ce3
Edward Leslie <edwardleslie09@gmail.com>
2019-04-28 01:20:50 -0400

    completed lesson 26

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,34 @@
+# print(1+2)
+# print(7*6)
+# print()
+# print("The End")
+# print("Python's strings are easy to use")
+# print('We can even include "quotes" in strings')
+# print("Hello" + " World")
+# greeting = "Hello"
+# name = "Bruce"
+# print(greeting + name)
+# #if we want a space, we can add that too
+# print(greeting + " " + name)
+
+# greeting = "Hello"
+# name = input("Please enter your name \n")
+# print(greeting + " " + name)
+
+splitString = "This string has \nbeen split over \nseveral \nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting"')
+print("The pet shop owner said \"No, no, \'e\'s uh,...he\'s resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no 'e's' uh,...he's resting"''')
+print("""The pet shop owner said "No, no 'e's' uh,...he's resting" """)
