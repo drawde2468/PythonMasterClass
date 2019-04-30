e03702fac2e5974b1dc2cf04cf9be90b259171cb
Edward Leslie <edwardleslie09@gmail.com>
2019-04-29 21:48:24 -0400

    completed lesson 28

--- Variables/Variable.py
+++ Variables/Variable.py
@@ -10,9 +10,9 @@
 # print(age)
 #
 # print(greeting + " " + str(age))
-
-a = 12
-b = 3
+#
+# a = 12
+# b = 3
 # print(a + b)
 # print(a - b)
 # print(a * b)
@@ -22,8 +22,50 @@ b = 3
 #
 # for i in range(1, a // b):
 #     print(i)
+#
+# print(a + b / 3 - 4 * 12)
+# print(a / 2 * 3)
+# print(8 * 3 / 2)
+# print((((a + b) / 3) - 4) * 12)
+#
+# c = a + b
+# d = c / 3
+# e = d - 4
+# print(e * 12)
 
-print(a + b / 3 - 4 * 12)
-print(a / 2 * 3)
-print(8 * 3 / 2)
-print((((a + b) / 3) - 4) * 12)
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+print(parrot[-3])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+number = "9,223,372,036,865,775,807"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+string1 = "He's "
+string2 = "probably "
+print(string1 + string2)
+print("he's " "probably " "pining")
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("thurs" in today)
+print("parrot" in "fjord")
+
+
+
+
+
+
+
+
+
