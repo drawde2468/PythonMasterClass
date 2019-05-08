835795c69bc6ba1a30fe5e26063379bdbf8522af
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 13:24:20 -0400

    completed lesson 50

---
+++ Tuples/tuples.py
@@ -0,0 +1,26 @@
+# t = ("a", "b", "c")
+# print(t)
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Imilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+# print(metallica)
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+#
+# for i in range(0 , len(metallica)):
+#     print(metallica[i])
+
+metallica2 = ["Ride the Lightning", "Metallica", 1984]
+print(metallica2)
+
+title, artist, year = imelda
+print(title)
+print(artist)
+print(year)
