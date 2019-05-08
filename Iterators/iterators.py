0c6384176d2b3c926ed2e7081f9d34c17a490e98
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 12:31:24 -0400

    completed lesson 48

---
+++ Iterators/iterators.py
@@ -0,0 +1,31 @@
+string = "1234567890"
+
+# for char in string:
+#     print(char)
+
+# my_iterator = iter(string)
+# print(my_iterator)
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+
+# for char in string:
+#     print(char)
+#
+# for char in iter(string):
+#     print(char)
+
+my_iterator = iter(string)
+
+for i in range(0, len(string)):
+    print(next(my_iterator))
+
+
+
