f9a21a445f4659024b0dbaeb04598b912ffcaec4
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 15:28:25 -0400

    completed lesson 51

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -6,21 +6,46 @@
 welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
 bad = "Bad Company", "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Imilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
-
-# print(metallica)
-# print(metallica[0])
-# print(metallica[1])
-# print(metallica[2])
+# imelda = "More Mayhem", "Imilda May", 2011, (1, "Pulling the Rug"), (2, "Psycho"),(3, "Mayhem"),(4, "Kentish Town Waltz")
 #
-# for i in range(0 , len(metallica)):
-#     print(metallica[i])
+# print(imelda)
+#
+# title, artist, year, track1, track2, track3, track4 = imelda
+# print(title)
+# print(artist)
+# print(year)
+# print(track1)
+# print(track2)
+# print(track3)
+# print(track4)
+##
 
-metallica2 = ["Ride the Lightning", "Metallica", 1984]
-print(metallica2)
+# imelda = "More Mayhem", "Imilda May", 2011, (
+#     (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+#
+# title, artist, year, tracks = imelda
+# # track1, track2, track3, track4 = tracks
+#
+# print(title)
+# print(artist)
+# print(year)
+# for track in tracks:
+#     print("\t{0}.  {1}".format(track[0], track[1]))
+##
 
-title, artist, year = imelda
+imelda = "More Mayhem", "Imilda May", 2011, [
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz")]
+
+print(imelda)
+
+imelda[3].append((5, "All For You"))
+
+title, artist, year, tracks = imelda
+tracks.append((6,"Eternity"))
 print(title)
 print(artist)
 print(year)
+for song in tracks:
+    track, title = song
+    print("\tTrack number {0}, Title: {1}".format(track, title))
+
