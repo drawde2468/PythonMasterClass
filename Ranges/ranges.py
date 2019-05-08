82ed0577652f2e26e065d9b948cf19232c8111dd
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 12:58:32 -0400

    completed lesson 49

--- Ranges/ranges.py
+++ Ranges/ranges.py
@@ -33,18 +33,58 @@
 # print(my_range.index(4))
 ##
 
-decimals = range(0, 100)
-print(decimals)
+# decimals = range(0, 100)
+# print(decimals)
+#
+# my_range = decimals[3:40:3]
+# print(my_range)
+#
+# for i in my_range:
+#     print(i)
+#
+# print('=' * 40)
+#
+# for i in range(3, 40, 3):
+#     print(i)
+#
+# print(my_range == range(3, 40, 3))
+##
 
-my_range = decimals[3:40:3]
-print(my_range)
+# decimals = range(0, 100)
+# my_range = decimals[3:40:3]
+# print(my_range == range(3, 40, 3))
+# print(range(0, 5, 2) == range(0, 6, 2))
+#
+# print(list(range(0, 5, 2)))
+# print(list(range(0, 6, 2)))
+#
+# r = range(0, 100)
+# print(r)
+#
+# for i in r[::-2]:
+#     print(i)
+# print('=' * 50)
+# for i in range(99, 0, -2):
+#     print(i)
+#
+# print('=' * 50)
+# print(range(0, 100)[::-2] == range(99, 0, -2))
+# # following will not run because it starts at 0 and tries to decrement 2 until it reached 100 which never happens
+# for i in range(0,100, -2):
+#     print(i)
+##
 
-for i in my_range:
+# backString = "egaugnal lufrewop yrev a si nohtyP"
+# print(backString[::-1])
+#
+# r = range(0,10)
+# for i in r[::-1]:
+#     print(i)
+##
+
+o = range(0, 100, 4)
+print(o)
+p = o[::5]
+print(p)
+for i in p:
     print(i)
-
-print('=' * 40)
-
-for i in range(3, 40, 3):
-    print(i)
-
-print(my_range == range(3, 40, 3))
