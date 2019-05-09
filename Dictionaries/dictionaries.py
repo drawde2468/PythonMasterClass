2055c65df0e4376c62f79c5c137d14bf72d9db42
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 20:47:09 -0400

    completed lesson 58

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -20,14 +20,46 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 #
 # # print(fruit["tomato"])
 
-print(fruit)
+# print(fruit)
 
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We don't have a " + dict_key)
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     description = fruit.get(dict_key, "We don't have a {}".format(dict_key))
+#     print(description)
+#     # if dict_key in fruit:
+#     #     description = fruit.get(dict_key)
+#     #     print(description)
+#     # else:
+#     #     print("We don't have a " + dict_key)
+##
+# for snack in fruit:
+#     print(fruit[snack])
+##
+# for i in range(10):
+#     for snack in fruit:
+#         print(snack + " is " + fruit[snack])
+#     print("=" * 40)
+##
+# orderedKeys = list(fruit.keys())
+# orderedKeys.sort()
+# orderedKeys = sorted(list(fruit.keys()))
+# for key in orderedKeys:
+#     print("{0}: {1}".format(key, fruit[key]))
+##
+# for key in sorted(fruit.keys()):
+#     print("{0}: {1}".format(key, fruit[key]))
+# # better to use keys, it's more efficient
+# for val in fruit.values():
+#     print(val)
+# print("=" * 40)
+# for key in fruit:
+#     print(fruit[key])
+# #
+
+fruit_keys = fruit.keys()
+print(fruit_keys)
+
+fruit["tomato"] = "not nice with ice cream"
+print(fruit_keys)
