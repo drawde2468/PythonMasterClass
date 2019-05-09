3c284b60ba0cd3df181fe5b4cbd972bf3103a59f
Edward Leslie <edwardleslie09@gmail.com>
2019-05-08 21:49:10 -0400

    completed lesson 59

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -58,8 +58,19 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
 #     print(fruit[key])
 # #
 
-fruit_keys = fruit.keys()
-print(fruit_keys)
+# fruit_keys = fruit.keys()
+# print(fruit_keys)
+#
+# fruit["tomato"] = "not nice with ice cream"
+# print(fruit_keys)
+# #
+print(fruit)
+print(fruit.items())
+f_tuple = tuple(fruit.items())
+print(f_tuple)
+for snack in f_tuple:
+    item, description = snack
+    print("{a} is {b}".format(a=item, b=description))
 
-fruit["tomato"] = "not nice with ice cream"
-print(fruit_keys)
+print(dict(f_tuple))
+
