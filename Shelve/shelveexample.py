0aba8775f8229bcfd14cf43c69ac08fc4506d428
Edward Leslie <edwardleslie09@gmail.com>
2019-05-18 18:50:21 -0400

    completed lesson 72

--- Shelve/shelveexample.py
+++ Shelve/shelveexample.py
@@ -2,14 +2,45 @@ import shelve
 
 # with shelve.open('ShelfTest') as fruit:
 fruit = shelve.open('ShelfTest')
-fruit["orange"] = "a sweet, orange, citrus fruit"
-fruit["apple"] = "good for making cider"
-fruit["lemon"] = "a sour, yellow citrus fruit"
-fruit["grape"] = "a small, sweet fruit growing in bunches"
-fruit["lime"] = "a sour, green citrus fruit"
+# fruit["orange"] = "a sweet, orange, citrus fruit"
+# fruit["apple"] = "good for making cider"
+# fruit["lemon"] = "a sour, yellow citrus fruit"
+# fruit["grape"] = "a small, sweet fruit growing in bunches"
+# fruit["lime"] = "a sour, green citrus fruit"
 
-print(fruit["lemon"])
-print(fruit["grape"])
+# print(fruit["lemon"])
+# print(fruit["grape"])
+#
+# fruit["lime"] = "great with tequila"
+#
+# for key in fruit:
+#     print(key + ": " + fruit[key])
+
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#
+#     if dict_key in fruit:
+#         description = fruit[dict_key]
+#         print(description)
+#     else:
+#         print("We dont have a {}".format(dict_key))
+
+# ordered_keys = sorted(list(fruit.keys()))
+#
+# for f in ordered_keys:
+#     print("{} - {}".format(f, fruit[f]))
+
+for v in fruit.values():
+    print(v)
+
+print(fruit.values())
+
+for f in fruit.items():
+    print(f)
+
+print(fruit.items())
+
+
 fruit.close()
-
-print(fruit)
