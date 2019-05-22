04f712bf6619ffb8e7bce763d9c626946c5601be
Edward Leslie <edwardleslie09@gmail.com>
2019-05-22 19:54:32 -0400

    completed lesson 97

---
+++ Functions/functions.py
@@ -0,0 +1,15 @@
+def python_food():
+    width = 80
+    text = "Spam and eggs"
+
+
+def center_text(text):
+    text = str(text)
+    left_margin = (80 - len(text)) // 2
+    print(" " * left_margin, text)
+
+
+center_text("spam and eggs")
+center_text(13)
+center_text("spam, spam, and eggs")
+center_text("spam, spam, spam, and eggs")
