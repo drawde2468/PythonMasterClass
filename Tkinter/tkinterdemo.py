4caedfc2b5f945c99bc57e832bd8edbfb76069d1
Edward Leslie <edwardleslie09@gmail.com>
2019-05-22 17:32:55 -0400

    completed lesson 91

--- Tkinter/tkinterdemo.py
+++ Tkinter/tkinterdemo.py
@@ -3,13 +3,29 @@ try:
 except ImportError:  # python 2
     import Tkinter as tkinter
 
-print(tkinter.TkVersion)
-print(tkinter.TclVersion)
-
 # tkinter._test()
 
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello World")
 mainWindow.geometry('640x480+300+200')
+
+label = tkinter.Label(mainWindow, text="Hello World")
+label.pack(side='top')
+
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+canvas = tkinter.Canvas(leftFrame, relief='raise', borderwidth=1)
+canvas.pack(side='left', anchor='n')
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.pack(side='right', anchor='n', expand=True)
+button1 = tkinter.Button(rightFrame, text="button1")
+button2 = tkinter.Button(rightFrame, text="button2")
+button3 = tkinter.Button(rightFrame, text="button3")
+
+button1.pack(side='top', anchor='n')
+button2.pack(side='top', anchor='s')
+button3.pack(side='top', anchor='e')
+
 mainWindow.mainloop()
