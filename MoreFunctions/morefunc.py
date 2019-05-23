d9203bfd003ee22c2cfa690cc302f3de78233b8b
Edward Leslie <edwardleslie09@gmail.com>
2019-05-22 21:00:59 -0400

    completed lesson 101

--- MoreFunctions/morefunc.py
+++ MoreFunctions/morefunc.py
@@ -9,13 +9,14 @@ def parabola(x):
     return y
 
 
-def draw_axes(canvas):
-    canvas.update()
-    x_origin = canvas.winfo_width() / 2
-    y_origin = canvas.winfo_height() / 2
-    canvas.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
-    canvas.create_line(-x_origin, 0, x_origin, 0, fill="black")
-    canvas.create_line(0, y_origin, 0, -y_origin, fill="black")
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
+    print(locals())
 
 
 def plot(canvas, x, y):
@@ -27,10 +28,15 @@ mainWindow = tkinter.Tk()
 mainWindow.title("Parabola")
 mainWindow.geometry("960x540")
 
-canvas = tkinter.Canvas(mainWindow, width=960, height=540)
+canvas = tkinter.Canvas(mainWindow, width=960 / 2, height=540)
 canvas.grid(row=0, column=0)
 
+canvas2 = tkinter.Canvas(mainWindow, width=960 / 2, height=540, background="blue")
+canvas2.grid(row=0, column=1)
+
+print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
+draw_axes(canvas2)
 
 for x in range(-100, 101):
     y = parabola(x)
