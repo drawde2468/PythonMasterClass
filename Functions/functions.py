6a8669682572a6fae734d24cb412d2e34f0c2613
Edward Leslie <edwardleslie09@gmail.com>
2019-05-22 20:27:13 -0400

    completed lesson 99

--- Functions/functions.py
+++ Functions/functions.py
@@ -3,18 +3,32 @@ def python_food():
     text = "Spam and eggs"
 
 
-def center_text(*args, sep=' ', end='\n', file=None, flush=False):
+def center_text(*args, sep=' '):
     text = ""
     for arg in args:
         text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text, end=end, file=file, flush=flush)
+    return " " * left_margin + text
 
 
-center_text("spam and eggs")
-center_text(13)
-center_text("spam, spam, and eggs")
-center_text("spam, spam, spam, and eggs")
+# with open("centered", 'w') as centered_file:
+# s1 = center_text("spam and eggs")
+# print(s1)
+# s2 = center_text(13)
+# print(s2)
+# s3 = center_text("spam, spam, and eggs")
+# print(s3)
+# s4 = center_text("spam, spam, spam, and eggs")
+# print(s4)
+# s5 = center_text("first", "second", 3, 4, "spam", sep=':')
+# print(s5)
 
-center_text("first", "second", 3, 4, "spam", sep=':')
-print()
+with open('menu', 'w') as menu:
+    s1 = center_text("spam and eggs")
+    print(s1, file=menu)
+    s2 = center_text(13)
+    print(s2, file=menu)
+    print(center_text("spam, spam, and eggs"), file=menu)
+    print(center_text("spam, spam, spam, and eggs"), file=menu)
+    s5 = center_text("first", "second", 3, 4, "spam", sep=':')
+    print(s5, file=menu)
